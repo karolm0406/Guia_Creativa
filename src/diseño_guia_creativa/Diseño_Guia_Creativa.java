42d4a0e6b6f7f96b5e743ac49c58e7dd9abb3e42
karolm0406 <kherreram2200@alumno.ipn.mx>
2024-04-02 17:42:55 -0600

    Primer commit

---
+++ src/diseño_guia_creativa/Diseño_Guia_Creativa.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package diseño_guia_creativa;
+
+/**
+ *
+ * @author yoyis
+ */
+public class Diseño_Guia_Creativa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
